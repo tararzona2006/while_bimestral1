8f35049f9d268dbf256264780184509baf974375
tarazona2006 <andressuarez@colegioguanenta.edu.co>
2023-03-23 16:17:26 -0500

    se agregaron cambios en los codigos

---
+++ ejemplo2/ejercicio1.py
@@ -0,0 +1,33 @@
+# while 
+
+#x = 0
+#while x < 10:
+  #  print ("El valor de x es: ", x)
+ #   x += 1
+
+#num = int(input("escribe un numero positivo"))
+#while num < 0:
+ #   print("este es un numero negativo, prueba de nuevo")
+  #  num = int(input("escribe un numero positivo")) 
+#print ("el numero es: " , num)  
+
+# ejemplo de un menu
+
+num = int(input("menu: 1(ver numeros), 0 salir"))
+while num != 0: 
+
+    x = 0
+    while x < 10:
+        print ("El valor de x es: ", x)
+        x += 1 
+
+        print("saliendo,,,")
+        num = int(input("menu: 1(ver numeros), 0 (salir"))
+
+print("gracias")
+
+    
+
+
+
+
